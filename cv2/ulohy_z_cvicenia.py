64e5abd877afb62cc8935781c0833725d2969d64
VRrrrr <71821990+VRrrrr@users.noreply.github.com>
2021-02-23 14:02:47 +0100

    feat: ADT SimpleBarrier from cvicenie 2 implemented using Semaphore

---
+++ cv2/ulohy_z_cvicenia.py
@@ -0,0 +1,42 @@
+from time import sleep
+from fei.ppds import Event, print, Mutex, Semaphore, Thread
+from random import randint
+
+
+class SimpleBarrier:
+    def __init__(self, N):
+        self.N = N
+        self.cnt = 0
+        self.mutex = Mutex()
+        self.turnstile = Semaphore(0)
+
+    def wait(self):
+        self.mutex.lock()
+        self.cnt += 1
+        if self.cnt == self.N:
+            self.turnstile.signal()
+        self.mutex.unlock()
+        self.turnstile.wait()
+        self.turnstile.signal()
+
+
+def barrier_example(barrier, thread_id):
+    """Predpokladajme, ze nas program vytvara a spusta 5 vlakien,
+    ktore vykonavaju nasledovnu funkciu, ktorej argumentom je
+    zdielany objekt jednoduchej bariery
+    """
+    sleep(randint(1, 10) / 10)
+    print("vlakno %d pred barierou" % thread_id)
+    barrier.wait()
+    print("vlakno %d po bariere" % thread_id)
+
+
+# priklad pouzitia ADT SimpleBarrier
+sb = SimpleBarrier(5)
+threads = list()
+for i in range(5):
+    t = Thread(barrier_example, sb, i)
+    threads.append(t)
+
+for t in threads:
+    t.join()
