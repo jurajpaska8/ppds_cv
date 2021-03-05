e9f6b1469aadf8cdf08d6f4663b72e8085dcbec0
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-05 08:59:02 +0100

    feat(ADTLightswitch): implement ADT from lecture

---
+++ cv3/light_switch.py
@@ -0,0 +1,21 @@
+from fei.ppds import Mutex
+
+
+class LightSwitch(object):
+    def __init__(self):
+        self.mutex = Mutex()
+        self.cnt = 0
+
+    def lock(self, sem):
+        self.mutex.lock()
+        self.cnt += 1
+        if self.cnt == 1:
+            sem.wait()
+        self.mutex.unlock()
+
+    def unlock(self, sem):
+        self.mutex.lock()
+        self.cnt -= 1
+        if self.cnt == 0:
+            sem.signal()
+        self.mutex.unlock()
