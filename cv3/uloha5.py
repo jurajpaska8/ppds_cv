6ecfca4beb81f2c8a491b8ab84f168be2b83b2f0
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-08 23:58:38 +0100

    feat(r-w): finish uloha 5. Save writers accesses into list.

--- cv3/uloha5.py
+++ cv3/uloha5.py
@@ -18,3 +18,33 @@ readers_avg_time = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 itrs = 10
 # number of all accesses (readers and writers combined) in each iter
 break_iter = 100
+
+for r in readers_count:
+    print(f"START: Number of readers = {r}")
+    for time in readers_avg_time:
+        print(f"Avg read time = {time} ms")
+        # number of writer accesses for each setting
+        tmp_writers = 0
+        for itr in range(itrs):
+            print(f"iter = {itr}")
+            # document creating
+            doc = Document()
+            # threads creating
+            threads = []
+
+            # writer threads
+            for i in range(writers_count):
+                t = Thread(writer, doc, i, writers_time_average_ms, break_iter)
+                threads.append(t)
+
+            # reader threads
+            for i in range(r):
+                t = Thread(reader, doc, i, time, break_iter)
+                threads.append(t)
+
+            for t in threads:
+                t.join()
+
+            tmp_writers += doc.writers_access
+        print(f"Writer accesses for {r} readers and avg time {time} ms = {tmp_writers / 10} of {break_iter}")
+        writers_accesses.append(tmp_writers / itrs)
