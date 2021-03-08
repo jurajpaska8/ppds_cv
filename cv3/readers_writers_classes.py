0aaa99ae2261dcc85d8feb3e75962b45b7c06cbc
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-09 00:40:15 +0100

    feat(r-w): add another Semaphore to Document in order to use it as turnstile

--- cv3/readers_writers_classes.py
+++ cv3/readers_writers_classes.py
@@ -20,6 +20,9 @@ class Document:
     def __init__(self):
         self.switch = LightSwitch()
         self.room_empty = Semaphore(1)
+        # add another Semaphore in order to solve starvation in r-w problem
+        self.turn = Semaphore(1)
+        # counters
         self.access_counter = 0
         self.readers_access = 0
         self.writers_access = 0
