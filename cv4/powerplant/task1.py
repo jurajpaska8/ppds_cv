9b0b32bb1848ad671def454d58dcf574090a9249
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-11 22:00:37 +0100

    fix(powerplant): fix print string.

--- cv4/powerplant/task1.py
+++ cv4/powerplant/task1.py
@@ -5,7 +5,9 @@ from cv4.powerplant.lightswitch import LightSwitch
 
 
 def monitor(monitor_id, valid_data, turnstile, memory_lock, lightswitch_monitor):
+    # monitor waiting for at least one sensor access
     valid_data.wait()
+
     while True:
         # wait 0.5s for actualization
         sleep(0.5)
@@ -17,9 +19,10 @@ def monitor(monitor_id, valid_data, turnstile, memory_lock, lightswitch_monitor)
         turnstile.signal()
 
         # ... MONITOR CODE ... #
-        print('monitor "%02d": monitor count=%02d\n' % monitor_id, monitor_count)
+        print(f'MONITOR {monitor_id}: monitor count={monitor_count}')
+
         # unlock memory lock if monitor is the last one
-        lightswitch_monitor.lock(memory_lock)
+        lightswitch_monitor.unlock(memory_lock)
 
 
 def sensor(sensor_id, valid_data, turnstile, memory_lock, lightswitch_sensor):
@@ -30,13 +33,16 @@ def sensor(sensor_id, valid_data, turnstile, memory_lock, lightswitch_sensor):
 
         # lock memory with ADT Lightswitch - multiple sensors can write simultaneously
         sensors_count = lightswitch_sensor.lock(memory_lock)
-        #
+
+        # ... SENSOR CODE ... #
         write_time = randint(10, 15)
-        print('sensor "%02d": writing for =%02d seconds. Sensors count=%02d\n' % sensor_id, write_time, sensors_count)
+        print(f'SENSOR {sensor_id}: writing for {write_time} seconds. Sensors count={sensors_count}')
         sleep(write_time)
 
         # signalization - at least one sensor updated data
         valid_data.signal()
+        # unlock memory
+        lightswitch_sensor.unlock(memory_lock)
 
 
 if __name__ == '__main__':
@@ -46,11 +52,11 @@ if __name__ == '__main__':
     switch_monitor = LightSwitch()
     switch_sensor = LightSwitch()
 
-    sensor_count = 11
-    monitor_count = 2
+    sensor_cnt = 11
+    monitor_cnt = 2
 
-    sensors = [Thread(sensor, i, vd, turn, mem_lock, switch_sensor) for i in range(sensor_count)]
-    monitors = [Thread(monitor, i, vd, turn, mem_lock, switch_monitor) for i in range(monitor_count)]
+    sensors = [Thread(sensor, i, vd, turn, mem_lock, switch_sensor) for i in range(sensor_cnt)]
+    monitors = [Thread(monitor, i, vd, turn, mem_lock, switch_monitor) for i in range(monitor_cnt)]
 
     for s in sensors:
         s.join()
