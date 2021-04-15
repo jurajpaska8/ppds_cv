47923440e5c29b7ac06d75c44467c40c3fbad2e3
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-04-15 23:47:07 +0200

    feat(sync): implement synchronous file write with wait

--- cv8/zadanie_sync.py
+++ cv8/zadanie_sync.py
@@ -25,7 +25,7 @@ async def write(tid, shared, afp):
 
 
 async def main():
-    async with async_open("text_sync.txt", 'w+') as afp:
+    async with async_open("text_async.txt", 'w+') as afp:
         shared = Shared()
         a = write(0, shared, afp)
         b = write(1, shared, afp)
@@ -35,5 +35,39 @@ async def main():
         print(f"\nTotal elapsed time: {elapsed:.1f}")
 
 
+def write_sync(tid, shared, fp):
+    time_start = time.perf_counter()
+    while shared.counter < 10:
+        print(f'id: {tid}, ctr: {shared.counter}')
+        tmp = shared.list.pop(0)
+        time.sleep(1)
+        shared.counter += 1
+        fp.write(f"id: {tid}: ctr: {shared.counter} time: {time.perf_counter() - time_start :.1f}, val:{tmp}\n")
+        yield
+    print(f'ctr: {shared.counter} time {time.perf_counter() - time_start :.1f}')
+
+
+def main_sync():
+    with open("text_sync.txt", 'w+') as fp:
+        shared = Shared()
+        a = write_sync(0, shared, fp)
+        b = write_sync(1, shared, fp)
+        tasks = [a, b]
+
+        start_time = time.perf_counter()
+        done = False
+        while not done:
+            for t in tasks:
+                try:
+                    next(t)
+                except StopIteration:
+                    tasks.remove(t)
+                if len(tasks) == 0:
+                    done = True
+        elapsed = time.perf_counter() - start_time
+        print(f"\nTotal elapsed time: {elapsed:.1f}")
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    #asyncio.run(main())
+    main_sync()
