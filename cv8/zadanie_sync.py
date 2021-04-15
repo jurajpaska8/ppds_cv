117ab0b5022593301a1dae951d84327f7cee0811
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-04-16 00:05:33 +0200

    refactor(async): remove unused queue

--- cv8/zadanie_sync.py
+++ cv8/zadanie_sync.py
@@ -7,14 +7,12 @@ from aiofile import async_open
 class Shared:
     def __init__(self):
         self.counter = 0
-        self.list = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
 async def write(tid, shared, afp):
     time_start = time.perf_counter()
     while shared.counter < 10:
         print(f'id: {tid}, ctr: {shared.counter}')
-        tmp = shared.list.pop(0)
         shared.counter += 1
         await afp.write(f"id: {tid}, ctr: {shared.counter}\n" + f"{shared.counter - 1}" * 100000000 + "\n")
     print(f'ctr: {shared.counter} time {time.perf_counter() - time_start :.1f}')
@@ -38,7 +36,6 @@ def write_sync(tid, shared, fp):
     time_start = time.perf_counter()
     while shared.counter < 10:
         print(f'id: {tid}, ctr: {shared.counter}')
-        tmp = shared.list.pop(0)
         shared.counter += 1
         fp.write(f"id: {tid}, ctr: {shared.counter}\n" + f"{shared.counter - 1}"*100000000 + "\n")
         yield
