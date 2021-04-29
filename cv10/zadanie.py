26c78ac39eb5505264633ca218b868aefc917d92
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-04-29 10:41:01 +0200

    refactor(cuda): delete unused imports

--- cv10/zadanie.py
+++ cv10/zadanie.py
@@ -1,10 +1,8 @@
 from time import perf_counter
 
 from numba import cuda
-import math
 from matplotlib import image
 from matplotlib import pyplot
-import numpy as np
 
 
 @cuda.jit
@@ -15,10 +13,10 @@ def change_pixels(io_array):
     :return: -
     """
     x = cuda.grid(1)
-    if x < io_array.size:
-        io_array[x, 0] *= 0.1
-        io_array[x, 1] *= 0.1
-        io_array[x, 2] *= 0.1
+    #if x < io_array.size:
+    io_array[x, 0] *= 0.1
+    io_array[x, 1] *= 0.1
+    io_array[x, 2] *= 0.1
 
 
 # change pixels
@@ -26,10 +24,6 @@ data = image.imread('chrome2x-8bit.png')
 # show image
 pyplot.imshow(data)
 pyplot.show()
-# threadsperblock = (32, 32)
-# blockspergrid_x = math.ceil(data.shape[0] / threadsperblock[0])
-# blockspergrid_y = math.ceil(data.shape[1] / threadsperblock[1])
-# blockspergrid = (blockspergrid_x, blockspergrid_y)
 
 # compute pixels and show
 data_gpu = []
@@ -45,7 +39,7 @@ for k in range(64):
     data_gpu.append(cuda.to_device(data[k], stream=streams[k]))
 
 for k in range(64):
-    change_pixels[1, 64, streams[k]](data_gpu[k])
+    change_pixels[2, 32, streams[k]](data_gpu[k])
 
 for k in range(64):
     gpu_out.append(data_gpu[k].copy_to_host(stream=streams[k]))
